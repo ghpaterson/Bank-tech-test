8bf5cb5f98a470f885dae0c9e7c45ee993782704
Graeme Paterson <ghpaterson@gmail.com>
2023-01-26 13:39:46 +0000

    tests for creating a Transaction Object

---
+++ transaction.test.js
@@ -0,0 +1,15 @@
+describe("Creates a transaction object", () => {
+  it("deposit, amount and transaction date", () => {
+    const trans = new Transaction("credit", 1000, "10-01-2023");
+    expect(trans.type).toBe("credit");
+    expect(trans.amount).toBe(1000);
+    expect(trans.date).toBe("10-01-2023");
+  });
+
+  it("withdrawal, amount and transaction date", () => {
+    const trans = new Transaction("debit", 1000, "10-01-2023");
+    expect(trans.type).toBe("debit");
+    expect(trans.amount).toBe(1000);
+    expect(trans.date).toBe("10-01-2023");
+  });
+});
