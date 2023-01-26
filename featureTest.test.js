2e966a7a1bbb29e0ea223a06ed9790956deaca55
Graeme Paterson <ghpaterson@gmail.com>
2023-01-26 14:32:15 +0000

    beforeEach for creating new Account object

--- featureTest.test.js
+++ featureTest.test.js
@@ -2,8 +2,12 @@ const Account = require("./account");
 const Transaction = require("./transaction");
 
 describe("Prints a formatted bank statement", () => {
+  let account;
+  beforeEach(() => {
+    account = new Account();
+  });
+
   it("A deposit of 1000 made on 10-01-2023", () => {
-    const account = new Account();
     const trans = new Transaction("credit", 1000, "10-01-2023");
     account.addTransaction(trans);
     expect(account.transactions).toEqual([
