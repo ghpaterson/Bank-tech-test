22d2108c96a5a1634e039abf199c2a316ea37929
Graeme Paterson <ghpaterson@gmail.com>
2023-01-26 13:56:42 +0000

    created Account unit test to create Account object

---
+++ account.test.js
@@ -0,0 +1,7 @@
+describe("Creates an Account Object", () => {
+  it("creates an empty transactions array and a current balance of ZERO", () => {
+    const account = new Account();
+    expect(account.transactions).toEqual([]);
+    expect(account.currentBalance).toEqual(0);
+  });
+});
